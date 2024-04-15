204c3a7a6f4830498feef50d2dd1ddb5cd55885c
cmwema <calebmwema65@gmail.com>
2024-04-15 13:49:17 +0300

    (new)-add route protection

--- predictions/views.py
+++ predictions/views.py
@@ -1,9 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from .utils import get_predicted_value, helper
 
 
 class Predict(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         # Assuming the symptoms are sent in the request body as a comma-separated string
         symptoms = request.data.get("symptoms", "")
